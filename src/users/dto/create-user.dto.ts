bae0277080d6a493679825f906ea535b0ca91c6c
Andrew Vitrenko <avitrenko2004@gmail.com>
2025-10-18 15:08:47 +0200

    fix(`user`): - added setting base currency on creating user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import {
+  IsEmail,
+  IsISO4217CurrencyCode,
+  IsNotEmpty,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 import { PASSWORD_REGEX } from '@/shared/config/validation';
 
@@ -20,4 +26,8 @@ export class CreateUserDto {
       'Password must consist of at least 8 symbols, uppercase and lowercase letters and numbers',
   })
   password: string;
+
+  @IsString()
+  @IsISO4217CurrencyCode()
+  baseCurrency: string;
 }
